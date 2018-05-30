e24c226df7141e55b730d58c3d7d4ac3c14dbc91
aanciaes <m.anciaes@campus.fct.unl.pt>
2018-05-30 19:35:59 +0100

    Added dockerfile. Changed to hex function from apache and to SunJCE provider

--- src/main/java/unl/fct/srsc/HelloWorld.java
+++ src/main/java/unl/fct/srsc/HelloWorld.java
@@ -1,39 +1,37 @@
 package unl.fct.srsc;
 
-import org.bouncycastle.util.encoders.Hex;
+import org.apache.commons.codec.binary.Hex;
 import redis.clients.jedis.Jedis;
 
-import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
-import javax.crypto.IllegalBlockSizeException;
-import javax.crypto.NoSuchPaddingException;
 import javax.crypto.spec.SecretKeySpec;
-import java.security.InvalidKeyException;
 import java.security.Key;
-import java.security.NoSuchAlgorithmException;
-import java.security.NoSuchProviderException;
 
 public class HelloWorld {
 
-    public static void main(String[] args) throws NoSuchPaddingException, NoSuchAlgorithmException, NoSuchProviderException, InvalidKeyException, BadPaddingException, IllegalBlockSizeException {
-        Jedis cli = new Jedis("localhost", 6379);
-        Cipher c = c = Cipher.getInstance("blowfish/ECB/PKCS5Padding", "BC");
+    public static void main(String[] args) {
+        try {
+            Jedis cli = new Jedis("localhost", 6379);
+            Cipher c = c = Cipher.getInstance("blowfish/ECB/PKCS5Padding", "SunJCE");
 
-        Key keySecret = new SecretKeySpec("Passwords".getBytes(),"blowfish/ECB/PKCS5Padding");
+            Key keySecret = new SecretKeySpec("Passwords".getBytes(), "blowfish");
 
-        //Cipher mp + hash
-        c.init(Cipher.ENCRYPT_MODE, keySecret);
+            //Cipher mp + hash
+            c.init(Cipher.ENCRYPT_MODE, keySecret);
 
-        String message = "Miguel:Anciaes:94000:bajdjorazz";
-        byte[] ecryptedCore = c.doFinal(message.getBytes());
+            String message = "Miguel:Anciaes:94000:bajdjorazz";
+            byte[] ecryptedCore = c.doFinal(message.getBytes());
 
-        String key = "123";
-        byte[] encryptedKey = c.doFinal(key.getBytes());
+            String key = "123";
+            byte[] encryptedKey = c.doFinal(key.getBytes());
 
-        System.out.println("Encrypted Key: " + Hex.toHexString(encryptedKey));
-        System.out.println("Encrypted Value: " + Hex.toHexString(ecryptedCore));
+            System.out.println("Encrypted Key: " + Hex.encodeHexString(encryptedKey));
+            System.out.println("Encrypted Value: " + Hex.encodeHexString(ecryptedCore));
 
-        cli.set(Hex.toHexString(encryptedKey), Hex.toHexString(ecryptedCore));
-        System.out.println(cli.get(Hex.toHexString(encryptedKey)));
+            cli.set(Hex.encodeHexString(encryptedKey), Hex.encodeHexString(ecryptedCore));
+            System.out.println(cli.get(Hex.encodeHexString(encryptedKey)));
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
