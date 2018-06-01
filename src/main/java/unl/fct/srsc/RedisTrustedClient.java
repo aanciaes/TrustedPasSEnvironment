f9a999e522bee2dc413a83380a7ece7e94c8742a
aanciaes <m.anciaes@campus.fct.unl.pt>
2018-06-01 01:43:43 +0100

    Added HMAC integrity check

--- src/main/java/unl/fct/srsc/RedisTrustedClient.java
+++ src/main/java/unl/fct/srsc/RedisTrustedClient.java
@@ -4,18 +4,12 @@ import org.apache.commons.codec.DecoderException;
 import org.apache.commons.codec.binary.Hex;
 import redis.clients.jedis.Jedis;
 
-import javax.crypto.BadPaddingException;
-import javax.crypto.Cipher;
-import javax.crypto.IllegalBlockSizeException;
-import javax.crypto.NoSuchPaddingException;
+import javax.crypto.*;
 import javax.crypto.spec.SecretKeySpec;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.security.InvalidKeyException;
-import java.security.Key;
-import java.security.NoSuchAlgorithmException;
-import java.security.NoSuchProviderException;
+import java.security.*;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -25,13 +19,14 @@ public class RedisTrustedClient {
     private static Key keySecret = null;
     private static Cipher cipher = null;
 
+    private static final int COLUMN_NUMBER = 3;
+
 
     public static void main(String[] args) {
         try {
             setup();
 
             BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
             String command = "";
 
             while (!(command = br.readLine().trim()).equals("exit")) {
@@ -62,13 +57,13 @@ public class RedisTrustedClient {
     }
 
     private static void processInsert(BufferedReader br) throws IOException {
-        System.out.println("Insert First Name:\n");
+        System.out.println("Insert First Name:");
         String name = br.readLine().trim();
 
-        System.out.println("Insert Last Name:\n");
+        System.out.println("\nInsert Last Name:");
         String lastName = br.readLine().trim();
 
-        System.out.println("Insert Salary:\n");
+        System.out.println("\nInsert Salary:");
         String salary = br.readLine().trim();
 
         boolean inserted = jedisInsert(name, lastName, salary);
@@ -77,24 +72,20 @@ public class RedisTrustedClient {
         System.out.println("-----------\n");
     }
 
-    private static void processGetByName(BufferedReader br) throws InvalidKeyException, DecoderException, BadPaddingException, IllegalBlockSizeException, IOException {
+    private static void processGetByName(BufferedReader br) throws IOException {
         System.out.println("Search by Name:");
         String name = br.readLine().trim();
 
-        Set<String> rst = new HashSet();
+        try {
+            Set<String> rst = jedisGetByName(name);
 
-        String key = String.valueOf(name.hashCode());
-        Set<String> indexes = cli.smembers(key);
-        System.out.println("Number of entries: " + indexes.size());
+            for (String row : rst) {
+                System.out.println(row);
+            }
 
-        cipher.init(Cipher.DECRYPT_MODE, keySecret);
-
-        for (String id : indexes) {
-            rst.add(new String(cipher.doFinal(Hex.decodeHex(cli.get(id)))));
-        }
-
-        for (String row : rst) {
-            System.out.println(row);
+        }catch (Exception e) {
+            System.out.println("An error occured...");
+            e.printStackTrace();
         }
     }
 
@@ -110,12 +101,64 @@ public class RedisTrustedClient {
             String key = String.valueOf(row.hashCode());
             System.out.println("Key: " + key);
 
-            cli.set(key, Hex.encodeHexString(ecryptedCore));
+            row = Hex.encodeHexString(ecryptedCore);
+
+            Mac hMac = Mac.getInstance("HMacSHA1", "SunJCE");
+            hMac.init(keySecret);
+            String mac = Hex.encodeHexString(hMac.doFinal(row.getBytes()));
+
+            String rowIntegrity = String.format("%s:%s", row, mac);
+
+            cli.set(key, rowIntegrity);
             cli.sadd(String.valueOf(name.hashCode()), key);
 
             return true;
         } catch (Exception e) {
             return false;
         }
+    }
+
+    private static Set<String> jedisGetByName(String name) throws InvalidKeyException, DecoderException, BadPaddingException, IllegalBlockSizeException, NoSuchProviderException, NoSuchAlgorithmException {
+        Set<String> rst = new HashSet();
+
+        String key = String.valueOf(name.hashCode());
+        Set<String> indexes = cli.smembers(key);
+        System.out.println("Number of entries: " + indexes.size());
+
+        cipher.init(Cipher.DECRYPT_MODE, keySecret);
+
+        for (String id : indexes) {
+            String uncheckedRow = cli.get(id);
+
+            if (checkIntegrity(uncheckedRow)){
+                try {
+                    String row = decryptRow(uncheckedRow);
+                    rst.add(row);
+                }catch (Exception e){}
+            }
+        }
+        return rst;
+    }
+
+    private static boolean checkIntegrity (String row) throws DecoderException, NoSuchProviderException, NoSuchAlgorithmException, InvalidKeyException {
+
+        String[] columns = row.split("\\:");
+
+        Mac hMac = Mac.getInstance("HMacSHA1", "SunJCE");
+        hMac.init(keySecret);
+
+        byte[] integrity = Hex.decodeHex(columns[1]);
+
+        if (!MessageDigest.isEqual(hMac.doFinal(columns[0].getBytes()), integrity)) {
+            System.out.println("Integrity Broken");
+            return false;
+        }
+
+        System.out.println("Integrity OK");
+        return true;
+    }
+
+    private static String decryptRow (String row) throws DecoderException, BadPaddingException, IllegalBlockSizeException {
+        return new String(cipher.doFinal(Hex.decodeHex(row)));
     }
 }
