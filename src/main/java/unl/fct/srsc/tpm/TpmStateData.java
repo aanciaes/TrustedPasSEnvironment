5db90bcbf9f0df81863d94cc158c4950473853a1
rm.amaral <rm.amaral@campus.fct.unl.pt>
2018-06-09 01:15:47 +0100

    verify process TPM

--- src/main/java/unl/fct/srsc/tpm/TpmStateData.java
+++ src/main/java/unl/fct/srsc/tpm/TpmStateData.java
@@ -1,17 +1,30 @@
 package unl.fct.srsc.tpm;
 
 import java.io.*;
-import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
 
 public class TpmStateData {
 
     private static BufferedWriter p_stdin;
     private static Process p;
+    private static List<String> state = new ArrayList<String>(10);
 
     public static void main(String[] args) throws IOException {
+
         startTerminal();
         runCommand("ps -ef");
         print();
+
+        String line = state.get(1);
+        String[] newLine = line.split("\\s+");
+
+        if(!newLine[1].trim().equals("1") || !newLine[7].trim().equals("/sbin/init")){
+            System.out.println("Corrupted");
+        }
+        else{
+            System.out.println("All Ok!");
+        }
 
     }
 
@@ -22,12 +35,17 @@ public class TpmStateData {
     }
 
     private static void print() throws IOException {
+
         String line;
         BufferedReader br = new BufferedReader(new InputStreamReader(p.getInputStream()));
+        int n = 0;
 
-        while ((line = br.readLine()) != null) {
-            System.out.println( br.readLine() );
+        while ((line = br.readLine()) != null && n < 10) {
+
+            state.add(n++, line);
+            System.out.println(line);
         }
+
         br.close();
     }
 
@@ -35,6 +53,7 @@ public class TpmStateData {
 
         ProcessBuilder builder = new ProcessBuilder("/bin/bash");
         p = null;
+
         try {
             p = builder.start();
         } catch (IOException e) {
@@ -42,7 +61,5 @@ public class TpmStateData {
         }
         //get stdin of shell
         p_stdin = new BufferedWriter(new OutputStreamWriter(p.getOutputStream()));
-
     }
-
 }
